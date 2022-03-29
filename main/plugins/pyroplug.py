50d0c3a9847e3b8ab19d730f80ab99c3c543b001
akprivatebots <90455659+akprivatebots@users.noreply.github.com>
2022-03-29 09:35:12 +0530

    Update pyroplug.py

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -83,7 +83,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i):
                     chat_id=sender,
                     video=file,
                     caption=caption,
-                    supports_streaming=True,
+                    supports_streaming=False,
                     duration=duration,
                     thumb=thumb_path,
                     progress=progress_for_pyrogram,
